314037382bb12312dc94226b935064e11facdff9
shahvrushali22 <shahvrushali22@gmail.com>
2019-06-27 10:14:04 +0530

    Initial Setup Added

---
+++ constants/Colors.js
@@ -0,0 +1,25 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+
+  // OUR COLORS
+  primaryColor: '#191B2F',
+  accentColor: '',
+  placeholderColor: '',
+  accentGradientStart: '',
+  accentGradientEnd: '',
+  primaryGradientStart:'',
+  primaryGradientEnd:'',
+  headingColor: '',
+
+};
